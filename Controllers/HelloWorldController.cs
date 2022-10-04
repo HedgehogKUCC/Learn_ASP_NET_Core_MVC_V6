d002aff458973f1fab1b6439e8d9f5021b77c9cb
ChanCheng KU <ccc908925@gmail.com>
2022-10-04 16:47:52 +0800

    USE query

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
 {
@@ -11,9 +12,15 @@ namespace MvcMovie.Controllers
         }
 
         // GET: /HelloWorld/Welcome/
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            /*
+             *  使用 C# 選擇性參數功能來指出若未針對 numTimes 參數傳遞任何值時，該參數預設為 1。
+             *  使用 HtmlEncoder.Default.Encode 來保護應用程式免于惡意輸入，例如透過 JavaScript。
+             *  在 $"Hello {name}, NumTimes is: {numTimes}" 中使用 -> 字串插值。
+             */
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
