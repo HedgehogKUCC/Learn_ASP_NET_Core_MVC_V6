aeb3389bda0d18eb08687921da33517615994e89
ChanCheng KU <ccc908925@gmail.com>
2022-10-04 16:53:06 +0800

    USE parameters

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,14 +13,14 @@ namespace MvcMovie.Controllers
 
         // GET: /HelloWorld/Welcome/
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
             /*
              *  使用 C# 選擇性參數功能來指出若未針對 numTimes 參數傳遞任何值時，該參數預設為 1。
              *  使用 HtmlEncoder.Default.Encode 來保護應用程式免于惡意輸入，例如透過 JavaScript。
              *  在 $"Hello {name}, NumTimes is: {numTimes}" 中使用 -> 字串插值。
              */
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
         }
     }
 }
