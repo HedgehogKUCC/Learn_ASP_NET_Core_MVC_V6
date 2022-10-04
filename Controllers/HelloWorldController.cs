3d5eba1ccb5108088b852e6b77ba75c96bdb4cfb
ChanCheng KU <ccc908925@gmail.com>
2022-10-04 17:03:33 +0800

    ADD View HelloWorld/Index

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,22 +5,27 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
+
+        // GET: /HelloWorld/
+        // public string Index()
+        // {
+        //     return "This is my default action...";
+        // }
 
         // GET: /HelloWorld/Welcome/
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
-        {
-            /*
-             *  使用 C# 選擇性參數功能來指出若未針對 numTimes 參數傳遞任何值時，該參數預設為 1。
-             *  使用 HtmlEncoder.Default.Encode 來保護應用程式免于惡意輸入，例如透過 JavaScript。
-             *  在 $"Hello {name}, NumTimes is: {numTimes}" 中使用 -> 字串插值。
-             */
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
-        }
+        // public string Welcome(string name, int ID = 1)
+        // {
+        //     /*
+        //      *  使用 C# 選擇性參數功能來指出若未針對 numTimes 參數傳遞任何值時，該參數預設為 1。
+        //      *  使用 HtmlEncoder.Default.Encode 來保護應用程式免于惡意輸入，例如透過 JavaScript。
+        //      *  在 $"Hello {name}, NumTimes is: {numTimes}" 中使用 -> 字串插值。
+        //      */
+        //     return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+        // }
     }
 }
